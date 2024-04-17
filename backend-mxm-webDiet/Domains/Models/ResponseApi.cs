4801c49063b13ba857fefb5e4704c5da5f7feb3c
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-04-17 20:37:17 -0300

    develop: edit ResponseApi to possible null

--- backend-mxm-webDiet/Domains/Models/ResponseApi.cs
+++ backend-mxm-webDiet/Domains/Models/ResponseApi.cs
@@ -2,21 +2,21 @@
  
  public class ResponseApiModel
     {
-        public string id { get; set; }
-        public string @object { get; set; }
+        public string? id { get; set; }
+        public string? @object { get; set; }
         public int created { get; set; }
-        public string model { get; set; }
-        public string system_fingerprint { get; set; }
-        public List<Choice> choices { get; set; }
-        public Usage usage { get; set; }
+        public string? model { get; set; }
+        public string? system_fingerprint { get; set; }
+        public List<Choice>? choices { get; set; }
+        public Usage? usage { get; set; }
     }
  
  public class Choice
     {
-        public string text { get; set; }
+        public string? text { get; set; }
         public int index { get; set; }
-        public object logprobs { get; set; }
-        public string finish_reason { get; set; }
+        public object? logprobs { get; set; }
+        public string? finish_reason { get; set; }
     }
 
     
