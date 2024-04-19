1f0f6babd9bda7b9e389509a1145417a10e0c735
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-04-19 17:26:30 -0300

    feature200: delete dieta page and dieta route

--- frontend-mxm-webDiet/src/app/app.routes.ts
+++ frontend-mxm-webDiet/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './paginas/home/home.component';
-import { DietaComponent } from './paginas/dieta/dieta.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'dieta', component: DietaComponent },
 ];
