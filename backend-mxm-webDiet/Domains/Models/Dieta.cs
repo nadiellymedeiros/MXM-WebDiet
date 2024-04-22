01bddbebb123db2eaaf66f190c7ef49bb5c83fdd
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-04-22 09:24:27 -0300

    feature102: add dieta model

---
+++ backend-mxm-webDiet/Domains/Models/Dieta.cs
@@ -0,0 +1,11 @@
+namespace mxm_webDiet.Domains.Models;
+
+public class Dieta
+{
+public string? Nome { get; set; }
+public string? Idade { get; set; }
+public string? Altura { get; set; }
+public string? Peso { get; set; }
+public string? Sexo { get; set; }
+public string? Cpf { get; set; }
+}
